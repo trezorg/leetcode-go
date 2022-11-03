56bb1a22ebbc5ff8b2d59e3b15bc3a972d456da0
Igor Nemilentsev <trezorg@gmail.com>
2022-11-03 19:15:29 +0300

    19. Remove Nth Node From End of List

---
+++ 19. Remove Nth Node From End of List/removenodefromendoflist.go
@@ -0,0 +1,61 @@
+package removenodefromendoflist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func newList(values []int) *ListNode {
+	if len(values) == 0 {
+		return nil
+	}
+	head := &ListNode{Val: values[0], Next: nil}
+	last := head
+	for i := 1; i < len(values); i++ {
+		new := &ListNode{Val: values[i], Next: nil}
+		last.Next = new
+		last = new
+	}
+	return head
+}
+
+func (head *ListNode) Slice() []int {
+	res := []int{}
+	for head != nil {
+		res = append(res, head.Val)
+		head = head.Next
+	}
+	return res
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	length := 0
+	cur := head
+	for cur != nil {
+		length++
+		cur = cur.Next
+	}
+
+	if n > length {
+		return head
+	}
+
+	position, idx := length-n, 0
+	cur = head
+	var prev *ListNode = nil
+	for cur != nil {
+		if idx == position {
+			if prev == nil {
+				head = head.Next
+				break
+			}
+			prev.Next = cur.Next
+			break
+		}
+		idx++
+		prev = cur
+		cur = cur.Next
+	}
+	return head
+}
---
+++ 19. Remove Nth Node From End of List/removenodefromendoflist_test.go
@@ -0,0 +1,31 @@
+package removenodefromendoflist
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRemoveNthFromList(t *testing.T) {
+
+	data := []struct {
+		data   []int
+		n      int
+		result []int
+	}{
+		{[]int{1, 2, 3, 4, 5}, 2, []int{1, 2, 3, 5}},
+		{[]int{1}, 1, []int{}},
+		{[]int{1, 2}, 1, []int{1}},
+		{[]int{1, 2}, 2, []int{2}},
+		{[]int{}, 2, []int{}},
+	}
+
+	for _, v := range data {
+		t.Run(fmt.Sprintf("%v", v.data), func(t *testing.T) {
+			head := newList(v.data)
+			assert.Equal(t, v.result, removeNthFromEnd(head, v.n).Slice())
+		})
+	}
+
+}
