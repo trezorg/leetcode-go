ae10923202c9192d3e8fc416b3f0485369a932be
Igor Nemilentsev <trezorg@gmail.com>
2022-11-05 20:18:38 +0300

    438. Find All Anagrams in a String

--- 567. Permutation in String/perminstring.go
+++ 567. Permutation in String/perminstring.go
@@ -1,16 +1,18 @@
 package main
 
+const size = 'z' - 'a' + 1
+
 func checkInclusion(s1 string, s2 string) bool {
 	n, sn := len(s1), len(s2)
 	if sn < n {
 		return false
 	}
-	subStringCounts := [127]int{}
-	stringCounts := [127]int{}
+	subStringCounts := [size]int{}
+	stringCounts := [size]int{}
 
 	for i := 0; i < n; i++ {
-		subStringCounts[s1[i]-'0']++
-		stringCounts[s2[i]-'0']++
+		subStringCounts[s1[i]-'a']++
+		stringCounts[s2[i]-'a']++
 	}
 
 	if subStringCounts == stringCounts {
@@ -18,8 +20,8 @@ func checkInclusion(s1 string, s2 string) bool {
 	}
 
 	for i := 1; i < sn-n+1; i++ {
-		stringCounts[s2[i-1]-'0']--
-		stringCounts[s2[i+n-1]-'0']++
+		stringCounts[s2[i-1]-'a']--
+		stringCounts[s2[i+n-1]-'a']++
 		if subStringCounts == stringCounts {
 			return true
 		}
